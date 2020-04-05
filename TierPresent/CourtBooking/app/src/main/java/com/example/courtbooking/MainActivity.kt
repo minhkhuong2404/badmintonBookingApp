2a27cff880b3cecb7882cbb087183d24c0b4d091
Minh Khuong <khuonglu1999@gmail.com>
2020-04-05 22:29:21 +0700

    Update MainActivity.kt

--- TierPresent/CourtBooking/app/src/main/java/com/example/courtbooking/MainActivity.kt
+++ TierPresent/CourtBooking/app/src/main/java/com/example/courtbooking/MainActivity.kt
@@ -27,16 +27,14 @@ class MainActivity : AppCompatActivity(), CenterAdapter.CallbackInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         // findViewById
-        cityChooser = findViewById(R.id.sp_city) as Spinner
-        dateChooser = findViewById(R.id.et_date) as EditText
+        cityChooser = findViewById<Spinner>(R.id.sp_city)
+        dateChooser = findViewById<EditText>(R.id.et_date)
         //result = findViewById(R.id.tv_result) as TextView
         //result2 = findViewById(R.id.tv_result2) as TextView
 
         // City list store here
-        var cities = arrayOf("City#A", "City#B", "City#C") // For testing
+        val cities = arrayOf("City#A", "City#B", "City#C", "City#D", "City#E") // For testing
 
         // Show first item on the cities array on the chosenCity spinner
 
@@ -46,8 +44,9 @@ class MainActivity : AppCompatActivity(), CenterAdapter.CallbackInterface {
 
         cityChooser.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) { /* Just a place holder */ }
+
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                //result.text = cities.get(position) // For testing
+//                result.text = cities[position] // For testing
             }
         }
 
@@ -64,6 +63,7 @@ class MainActivity : AppCompatActivity(), CenterAdapter.CallbackInterface {
         // On click of date chooser
 
         dateChooser.setOnClickListener {
+
             // Setting up date picker dialog
             val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ _: DatePicker?, mYear: Int, mMonth: Int, mDay: Int ->
                 // Add 1, since in Kotlin start from 0 - 11
@@ -72,17 +72,21 @@ class MainActivity : AppCompatActivity(), CenterAdapter.CallbackInterface {
                 et_date.setText("" + mDay + "/" + finalMonth + "/" + mYear)
                 //result2.text = et_date.text; // For testing
             }, year, month, day)
+            datePicker.datePicker.minDate = System.currentTimeMillis() - 1000
             // Show Date Dialog
             datePicker.show()
+
         }
 
-
-
         // On button clicked Show Available Slots
+        b_show_slots.setOnClickListener {
+            val selectedCity = cityChooser.selectedItem
+            val randomCenter = cities.indexOf(selectedCity) + (1..3).random()
+            initRecyclerViewVCenter(randomCenter)
+        }
         // On button clicked Show My Bookings
 
         // Initialize the CENTER recycler view
-        initRecyclerViewVCenter()
     }
     // Generate fake data for testing recycler view
     private fun getCenterCourtSlotList(size: Int): List<Center> {
@@ -95,30 +99,29 @@ class MainActivity : AppCompatActivity(), CenterAdapter.CallbackInterface {
         return list
     }
     private fun getCourtList(): List<Court> {
-        val list = listOf<Court>(Court("Court#1", getSlotList(1)), Court("Court#2", getSlotList(2)),
-            Court("Court#3", getSlotList(3)), Court("Court#4", getSlotList(4)),Court("Court#5", getSlotList(5)))
-        return list
+        return listOf(Court("Court#1", getSlotList(1)), Court("Court#2", getSlotList(2)),
+            Court("Court#3", getSlotList(3)), Court("Court#4", getSlotList(4)),Court("Court#5",
+                getSlotList(5)
+            ))
     }
     private fun getSlotList(type : Int): List<Slot> {
 
-        val slots = when {
-            type == 1 -> listOf<Slot>(Slot("7:00 - 8:00"), Slot("7:30 - 8:30"), Slot("8:30 - 9:00"),
+        return when (type) {
+            1 -> listOf(Slot("7:00 - 8:00"), Slot("7:30 - 8:30"), Slot("8:30 - 9:00"),
                 Slot("9:00 - 10:00"), Slot("10:00 - 11:00"), Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
-            type == 2 -> listOf<Slot>(Slot("7:30 - 8:30"), Slot("8:30 - 9:00"),
+            2 -> listOf(Slot("7:30 - 8:30"), Slot("8:30 - 9:00"),
                 Slot("9:00 - 10:00"), Slot("10:00 - 11:00"), Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
-            type == 3 -> listOf<Slot>(Slot("8:30 - 9:00"),
+            3 -> listOf(Slot("8:30 - 9:00"),
                 Slot("9:00 - 10:00"), Slot("10:00 - 11:00"), Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
-            type == 4 -> listOf<Slot>(Slot("9:00 - 10:00"), Slot("10:00 - 11:00"), Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
-            else -> listOf<Slot>(Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
+            4 -> listOf(Slot("9:00 - 10:00"), Slot("10:00 - 11:00"), Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
+            else -> listOf(Slot("12:00 - 13:00"), Slot("13:00 - 21:00"))
         }
-
-        return slots
     }
-    private fun initRecyclerViewVCenter(){
+    private fun initRecyclerViewVCenter(numOfCenter: Int){
         // Calling the recycler view for Center
         rv_center.apply {
             layoutManager = LinearLayoutManager(this@MainActivity, LinearLayout.VERTICAL, false)
-            adapter = CenterAdapter(getCenterCourtSlotList(27), this@MainActivity)
+            adapter = CenterAdapter(getCenterCourtSlotList(numOfCenter), this@MainActivity)
         }
     }
 
@@ -130,3 +133,4 @@ class MainActivity : AppCompatActivity(), CenterAdapter.CallbackInterface {
 
 
 }
+
