28dc8bba1b106299ade1a75ce8b31c28fc637c9e
Nguyễn Trí Nguyên <38081497+nguyentringuyencool@users.noreply.github.com>
2020-04-07 03:01:20 +0700

    update list of error codes

--- MySQL/finaldatabase.sql
+++ MySQL/finaldatabase.sql
@@ -575,4 +575,37 @@ CALL CreateBooking(22,"2020-03-29 09:27:18",'2020-04-19',9,00,00,10,00,00,'HCM',
 
 /* test 23 */
 CALL CreateBooking(23,"2020-03-29 09:27:18",'2020-04-19',9,00,00,10,00,00,'HCM','HCM_quan5','Court#X','Customer#X');
-/* error: CB-111
+/* error: CB-111 */
+		   
+		   
+/*list of error codes
+
+CC-001 | city existed 
+CCtr-001 | center existed 
+CCourt-001 | court existed 
+CPL-001 | player existed 
+CS-001 | staff existed
+
+
+CA-001 | customer does not exist 
+CA-002 | booking does not exist 
+CA-003 | this customer does not own the booking 
+CA-004 | violates 24 hours before start time
+
+
+CP-001 | staff does not exist 
+CP-002 | booking does not exist 
+CP-003 | this staff has no relationship with the booking (join booking - court - center - staff) 
+
+CB-001 | startTime < DATE(NOW())
+CB-002 | startTime < openTime 
+CB-003 | endTime > closeTime 
+CB-004 | endTime < startTime   
+CB-005 | playtime invalid (valid: 45m, 1h, 1h15m, 1h30m)  
+CB-006 | overlapping booking  
+CB-007 | Player has pending booking in past 
+CB-008 | Player can book no more than 3 bookings 
+CB-109 | Customer does not existed 
+CB-110 | Court does not existed
+CB-111 | Court & Customer do not existed
+*/
