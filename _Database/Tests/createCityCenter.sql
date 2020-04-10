dfb1cc3944bfa20f81e2472651ae06361e1f1ca4
yadsendew <44693107+yadsendew@users.noreply.github.com>
2020-04-10 16:05:47 +0700

    update result code

--- _Database/Tests/createCityCenter.sql
+++ _Database/Tests/createCityCenter.sql
@@ -14,26 +14,26 @@ Hướng dẫn kiểm thử cho: createCityCenter(centerId, cityId)
 /* Test if createCityCenter is accepted when centerId and cityId are valid */
 delete from city where city_id = "1";
 delete from center where center_id = "A" and city_id = "1";
-CALL createCity("1");
-CALL createCityCenter("A", "1");
+CALL createCity("1", @code);
+CALL createCityCenter("A", "1", @code);
 /* expected no error code */
 
 /* Tests if createCityCenter is rejected when cityId is invalid */
-CALL createCityCenter("A", "#");
+CALL createCityCenter("A", "#", @code);
 /* expected error CEN-000 */
 
 /* Tests if createCityCenter is rejected when centerId is invalid */
-CALL createCityCenter("#", "A");
+CALL createCityCenter("#", "A", @code);
 /* expected error CEN-001 */
 
 /* Tests if createCityCenter is rejected when cityId is not existed */
-CALL createCityCenter("A", "A");
+CALL createCityCenter("A", "A", @code);
 /* expected error CEN-002 */
 
 /* Tests if createCityCenter is rejected when centerId is existed */
 delete from city where city_id = "1";
 delete from center where center_id = "A" and city_id = "1";
-CALL createCity("1");
-CALL createCityCenter("A", "1");
-CALL createCityCenter("A", "1");
+CALL createCity("1", @code);
+CALL createCityCenter("A", "1", @code);
+CALL createCityCenter("A", "1", @code);
 /* expected error CEN-003 */
