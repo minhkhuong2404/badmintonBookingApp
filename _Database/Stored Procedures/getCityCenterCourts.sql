d4ff9acc72ffa4d30947de9848817e86716e0bdb
Nguyễn Trí Nguyên <38081497+nguyentringuyencool@users.noreply.github.com>
2020-04-22 16:52:45 +0700

    Update getCityCenterCourts.sql

--- _Database/Stored Procedures/getCityCenterCourts.sql
+++ _Database/Stored Procedures/getCityCenterCourts.sql
@@ -25,33 +25,3 @@ END IF;
 SELECT resultCode;
 end//
 DELIMITER ;
-
-/* Test if getCityCenterCourts is accepted when centerId and cityId are valid */
-delete from city where city_id = "HCM";
-delete from center where center_id = "A" and city_id = "HCM";
-delete from court where court_id = "1" and center_id = "A" and city_id = "HCM";
-CALL createCity("HCM",@code);
-CALL createCityCenter("A","HCM",@code);
-CALL createCityCenterCourt("1", "HCM", "A", @code);
-CALL getCityCenterCourts("A", "HCM", @code);
-/* expected no error code */
-
-/* Tests if getCityCenterCourts is rejected when cityId is invalid */
-CALL getCityCenterCourts("A", "#", @code);
-/* expected error GCCC-000 */
-
-/* Tests if getCityCenterCourts is rejected when centerId is invalid */
-CALL getCityCenterCourts("#", "A", @code);
-/* expected error GCCC-001 */
-
-/* Tests if getCityCenterCourts is rejected when cityId is not existed */
-CALL getCityCenterCourts("A", "A", @code);
-/* expected error GCCC-002 */
-
-/* Tests if getCityCenterCourts is rejected when centerId is not existed */
-CALL getCityCenterCourts("HCM", "HCM", @code);
-/* expected error GCCC-003 */
-
-
-
-
