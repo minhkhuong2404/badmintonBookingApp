c93a0c2d8c7ac8b1facab8a5af0be67561195f44
Nguyễn Trí Nguyên <38081497+nguyentringuyencool@users.noreply.github.com>
2020-04-22 16:44:24 +0700

    Create getCenterStaffs.sql

---
+++ _Database/Tests/getCenterStaffs.sql
@@ -0,0 +1,31 @@
+/* 
+[English]
+Test guide for: getCenterStaffs(centerId)
+- Ensure the database is empty when doing the tests (clean up staff, city, center will let database empty)
+- Every test has n SQL statements, the (n - 1) first SQL statements are the scenario for the test.
+- The last SQL statements is the actual test.
+[Tiếng Việt]
+Hướng dẫn kiểm thử cho: getCenterStaffs(centerId)
+- Đảm bảo rằng database hoàn toàn trống (xoá dữ liệu trong staff, city, center sẽ làm toàn bộ database trống)
+- Mỗi test có n câu lệnh SQL, (n - 1) câu lệnh đầu tiên dùng để tạo dữ liệu mẫu.
+- Câu lệnh SQL cuối cùng là câu lệnh dùng để test.
+*/ 
+
+/* Test if getCenterStaffs is valid and existed */
+delete from staff where staff_id = "S";
+delete from city where city_id = "HCM";
+delete from center where center_id = "A" and city_id = "HCM";
+CALL createCity("HCM",@code);
+CALL createCityCenter("A","HCM",@code);
+CALL createStaff("S", "HCM", "A", @code);
+CALL getCenterStaffs('A', @code);
+/* expected table staff and no error code */
+
+/* Test if getCenterStaffs is rejected when centerId is invalid */
+CALL getCenterStaffs("#", @code);
+/* expected error code GCS-000 */
+
+
+/* Test if getCenterStaffs is rejected when centerId is not existed */
+CALL getCenterStaffs("B", @code);
+/* expected error code GCS-001 */
