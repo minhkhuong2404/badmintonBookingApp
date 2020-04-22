07b63d9913b014780e5e5782dd06f5c13a752ac9
Nguyễn Trí Nguyên <38081497+nguyentringuyencool@users.noreply.github.com>
2020-04-22 16:51:45 +0700

    Update getCenterBookings.sql

--- _Database/Stored Procedures/getCenterBookings.sql
+++ _Database/Stored Procedures/getCenterBookings.sql
@@ -16,24 +16,3 @@ END IF;
 SELECT resultCode;
 end//
 DELIMITER ;
-
-/* Test if getCenterBookings is valid and existed, then return the information */
-delete from city where city_id = "HCM";
-delete from center where center_id = "A" and city_id = "HCM";
-delete from court where court_id = "1" and center_id = "A" and city_id = "HCM";
-delete from player where player_id = "P";
-delete from booking where booking_id = "booking1";
-CALL createCity("HCM",@code);
-CALL createCityCenter("A","HCM",@code);
-CALL createCityCenterCourt("1", "HCM", "A", @code);
-CALL createPlayer("P", @code);
-CALL createBooking("booking1", "2021-05-01", "10:00:00" , "10:45:00" , 'HCM', 'A', '1', 'P', @code);
-CALL getCenterBookings('A', @code);
-
-/* Test if getCenterBookings is rejected when centerId is invalid */
-call getCenterBookings('#', @code);
-/* expected error code GCB-000 */
-
-/* Test if getCenterBookings is rejected when centerId is not existed */
-call getCenterBookings('B', @code);
-/* expected error code GCB-001 */
