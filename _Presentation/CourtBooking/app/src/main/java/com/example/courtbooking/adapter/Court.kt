a21a27c3d3140037c7e18b456c5debde922963f4
yadsendew <44693107+yadsendew@users.noreply.github.com>
2020-04-25 16:57:43 +0700

    Update

--- _Presentation/CourtBooking/app/src/main/java/com/example/courtbooking/adapter/Court.kt
+++
@@ -1,3 +0,0 @@
-package com.example.courtbooking.adapter
-
-data class Court (var id: String, val slotList : List<Slot>)
--- _Presentation/CourtBooking/app/src/main/java/com/example/courtbooking/adapter/Slot.kt
+++
@@ -1,3 +0,0 @@
-package com.example.courtbooking.adapter
-
-data class Slot (var id: String)
