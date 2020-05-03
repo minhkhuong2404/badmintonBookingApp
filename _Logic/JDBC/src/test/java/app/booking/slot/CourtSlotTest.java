1d8b3a0b932d9357cd45a062b950e8e2c834aa66
Nguyễn Trí Nguyên <38081497+nguyentringuyencool@users.noreply.github.com>
2020-05-03 11:22:02 +0700

    pls send help

--- _Logic/JDBC/src/test/java/app/booking/slot/CourtSlotTest.java
+++ _Logic/JDBC/src/test/java/app/booking/slot/CourtSlotTest.java
@@ -836,4 +836,496 @@ class CourtSlotTest {
         assertEquals(true, expected.equals(actual));
     }
 
+    @Test
+    public void Construct_GivenDate_BookingAtStartOfDay() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+
+        String sql5 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt5 = ConnectionDB.getInstance().getConnection().prepareStatement(sql5);
+
+        smt5.setString(1, "1");
+        smt5.setString(2, "2020-04-22 18:23:18");
+        smt5.setString(3, "2020-05-10");
+        smt5.setString(4, "07:00:00");
+        smt5.setString(5, "07:45:00");
+        smt5.setString(6, "A");
+        smt5.setString(7, "A1");
+        smt5.setString(8, "A1C");
+        smt5.setString(9, "player1");
+        smt5.setString(10, "0");
+
+
+        smt5.execute();
+
+
+        CourtSlot courtSlot = new CourtSlot("A1C", Date.valueOf("2020-05-10"));
+
+        ArrayList<Slot> slots = courtSlot.getCourtSlots();
+
+        assertThat(slots.get(0)).isEqualToComparingFieldByField(new Slot("07:45:00", "21:00:00"));
+    }
+
+    @Test
+    public void Construct_GivenDate_BookingAtEndOfDay() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+
+        String sql5 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt5 = ConnectionDB.getInstance().getConnection().prepareStatement(sql5);
+
+        smt5.setString(1, "1");
+        smt5.setString(2, "2020-04-22 18:23:18");
+        smt5.setString(3, "2020-05-10");
+        smt5.setString(4, "20:15:00");
+        smt5.setString(5, "21:00:00");
+        smt5.setString(6, "A");
+        smt5.setString(7, "A1");
+        smt5.setString(8, "A1C");
+        smt5.setString(9, "player1");
+        smt5.setString(10, "0");
+
+
+        smt5.execute();
+
+
+        CourtSlot courtSlot = new CourtSlot("A1C", Date.valueOf("2020-05-10"));
+
+        ArrayList<Slot> slots = courtSlot.getCourtSlots();
+
+        assertThat(slots.get(0)).isEqualToComparingFieldByField(new Slot("07:00:00", "20:15:00"));
+    }
+
+    @Test
+    public void Construct_GivenDate_BookingAtStartAndEndOfDay() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+
+        String sql5 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt5 = ConnectionDB.getInstance().getConnection().prepareStatement(sql5);
+
+        smt5.setString(1, "1");
+        smt5.setString(2, "2020-04-22 18:23:18");
+        smt5.setString(3, "2020-05-10");
+        smt5.setString(4, "07:00:00");
+        smt5.setString(5, "07:45:00");
+        smt5.setString(6, "A");
+        smt5.setString(7, "A1");
+        smt5.setString(8, "A1C");
+        smt5.setString(9, "player1");
+        smt5.setString(10, "0");
+
+
+        smt5.execute();
+
+        String sql6 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt6 = ConnectionDB.getInstance().getConnection().prepareStatement(sql6);
+
+        smt6.setString(1, "2");
+        smt6.setString(2, "2020-04-22 18:23:18");
+        smt6.setString(3, "2020-05-10");
+        smt6.setString(4, "20:15:00");
+        smt6.setString(5, "21:00:00");
+        smt6.setString(6, "A");
+        smt6.setString(7, "A1");
+        smt6.setString(8, "A1C");
+        smt6.setString(9, "player1");
+        smt6.setString(10, "0");
+
+
+        smt6.execute();
+
+        CourtSlot courtSlot = new CourtSlot("A1C", Date.valueOf("2020-05-10"));
+
+        ArrayList<Slot> slots = courtSlot.getCourtSlots();
+
+        assertThat(slots.get(0)).isEqualToComparingFieldByField(new Slot("07:45:00", "20:15:00"));
+    }
+
+    @Test
+    public void Construct_GivenDate_SlotAtStartOfDay() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+
+        String sql5 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt5 = ConnectionDB.getInstance().getConnection().prepareStatement(sql5);
+
+        smt5.setString(1, "1");
+        smt5.setString(2, "2020-04-22 18:23:18");
+        smt5.setString(3, "2020-05-10");
+        smt5.setString(4, "07:45:00");
+        smt5.setString(5, "21:00:00");
+        smt5.setString(6, "A");
+        smt5.setString(7, "A1");
+        smt5.setString(8, "A1C");
+        smt5.setString(9, "player1");
+        smt5.setString(10, "0");
+
+
+        smt5.execute();
+
+
+        CourtSlot courtSlot = new CourtSlot("A1C", Date.valueOf("2020-05-10"));
+
+        ArrayList<Slot> slots = courtSlot.getCourtSlots();
+
+        assertThat(slots.get(0)).isEqualToComparingFieldByField(new Slot("07:00:00", "07:45:00"));
+    }
+
+    @Test
+    public void Construct_GivenDate_SlotAtEndOfDay() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+
+        String sql5 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt5 = ConnectionDB.getInstance().getConnection().prepareStatement(sql5);
+
+        smt5.setString(1, "1");
+        smt5.setString(2, "2020-04-22 18:23:18");
+        smt5.setString(3, "2020-05-10");
+        smt5.setString(4, "07:00:00");
+        smt5.setString(5, "20:15:00");
+        smt5.setString(6, "A");
+        smt5.setString(7, "A1");
+        smt5.setString(8, "A1C");
+        smt5.setString(9, "player1");
+        smt5.setString(10, "0");
+
+
+        smt5.execute();
+
+
+        CourtSlot courtSlot = new CourtSlot("A1C", Date.valueOf("2020-05-10"));
+
+        ArrayList<Slot> slots = courtSlot.getCourtSlots();
+
+        assertThat(slots.get(0)).isEqualToComparingFieldByField(new Slot("20:15:00", "21:00:00"));
+    }
+
+    @Test
+    public void Construct_GivenDate_SlotAtStartAndEndOfDay() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+
+        String sql5 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt5 = ConnectionDB.getInstance().getConnection().prepareStatement(sql5);
+
+        smt5.setString(1, "1");
+        smt5.setString(2, "2020-04-22 18:23:18");
+        smt5.setString(3, "2020-05-10");
+        smt5.setString(4, "07:45:00");
+        smt5.setString(5, "20:15:00");
+        smt5.setString(6, "A");
+        smt5.setString(7, "A1");
+        smt5.setString(8, "A1C");
+        smt5.setString(9, "player1");
+        smt5.setString(10, "0");
+
+
+        smt5.execute();
+
+
+        CourtSlot courtSlot = new CourtSlot("A1C", Date.valueOf("2020-05-10"));
+
+        ArrayList<Slot> slots = courtSlot.getCourtSlots();
+
+        assertThat(slots.get(0)).isEqualToComparingFieldByField(new Slot("07:00:00", "07:45:00"));
+        assertThat(slots.get(1)).isEqualToComparingFieldByField(new Slot("20:15:00", "21:00:00"));
+    }
+
+    @Test
+    public void Construct_GivenDate_IntervalSmallerThan45min() throws Exception {
+        // clean up database
+        SQLStatement.cleanTable("city");
+        SQLStatement.cleanTable("center");
+        SQLStatement.cleanTable("staff");
+        SQLStatement.cleanTable("court");
+        SQLStatement.cleanTable("booking");
+
+        //scenario
+
+        String sql = "INSERT INTO city VALUE (?)";
+
+        PreparedStatement smt = ConnectionDB.getInstance().getConnection().prepareStatement(sql);
+
+        smt.setString(1, "A");
+
+        smt.execute();
+
+        String sql2 = "INSERT INTO center VALUE (?, ?)";
+
+        PreparedStatement smt2 = ConnectionDB.getInstance().getConnection().prepareStatement(sql2);
+
+        smt2.setString(1, "A1");
+        smt2.setString(2, "A");
+
+        smt2.execute();
+
+        String sql3 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt3 = ConnectionDB.getInstance().getConnection().prepareStatement(sql3);
+
+        smt3.setString(1, "A1C");
+        smt3.setString(2, "A");
+        smt3.setString(3, "A1");
+
+        smt3.execute();
+
+        String sql9 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt9 = ConnectionDB.getInstance().getConnection().prepareStatement(sql9);
+
+        smt9.setString(1, "A1C1");
+        smt9.setString(2, "A");
+        smt9.setString(3, "A1");
+
+        smt9.execute();
+
+        String sql10 = "INSERT INTO court VALUE (?, ?, ?)";
+
+        PreparedStatement smt10 = ConnectionDB.getInstance().getConnection().prepareStatement(sql10);
+
+        smt10.setString(1, "A1C2");
+        smt10.setString(2, "A");
+        smt10.setString(3, "A1");
+
+        smt10.execute();
+
+
+        String sql7 = "INSERT INTO booking VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement smt7 = ConnectionDB.getInstance().getConnection().prepareStatement(sql7);
+
+        smt7.setString(1, "9");
+        smt7.setString(2, "2020-04-22 19:08:48");
+        smt7.setString(3, "2020-05-10");
+        smt7.setString(4, "07:30:00");
+        smt7.setString(5, "21:00:00");
+        smt7.setString(6, "A");
+        smt7.setString(7, "A1");
+        smt7.setString(8, "A1C1");
+        smt7.setString(9, "player1");
+        smt7.setString(10, "0");
+
+        smt7.execute();
+
+        CourtSlot courtSlot = new CourtSlot("A1C1", Date.valueOf("2020-05-10"));
+        // expected result
+        ArrayList<Slot> slots = new ArrayList<>();
+        ArrayList<Slot> slots2 = courtSlot.getCourtSlots();
+
+        assertEquals(slots, slots2);
+    }
+
 }
