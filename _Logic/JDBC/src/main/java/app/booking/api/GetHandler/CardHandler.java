f49ad28aa7853d6250424b37317272e62e50f847
haijames <45039771+haijames@users.noreply.github.com>
2020-05-05 10:10:42 +0700

    Fix CardHandler

--- _Logic/JDBC/src/main/java/app/booking/api/GetHandler/CardHandler.java
+++ _Logic/JDBC/src/main/java/app/booking/api/GetHandler/CardHandler.java
@@ -5,12 +5,14 @@ import app.booking.api.JsonConverter;
 import app.booking.api.ResponseEntity;
 import app.booking.api.StatusCode;
 import app.booking.db.Booking;
+import app.booking.db.Card;
 import app.booking.db.SQLStatement;
 import app.booking.errors.GlobalExceptionHandler;
 import app.booking.slot.CitySlot;
 
 import java.io.InputStream;
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -25,7 +27,7 @@ public class CardHandler extends GetHandler {
         Map<String, List<String>> params = this.getParameters();
 
         String PlayerId = params.get("id").get(0);
-        Date date = Date.valueOf(params.get("date").get(0));
+        Timestamp date = Timestamp.valueOf(params.get("date").get(0));
         // TODO: handle the case of missing/incorrect params
 
         // Get cards of the player
@@ -33,9 +35,9 @@ public class CardHandler extends GetHandler {
         //Check for usable card
         for (Card c:PlayerCard
              ) {
-            if (date.compareTo(c.exdate) > 0)
+            if (date.before(c.getExpire_date()))
                 PlayerCard.remove(c);
-            else if (c.numb == 0) PlayerCard.remove(c);
+            else if (c.getRemainBooking() == 0) PlayerCard.remove(c);
         }
         // Convert to json
         String response = JsonConverter.toJson(PlayerCard);
