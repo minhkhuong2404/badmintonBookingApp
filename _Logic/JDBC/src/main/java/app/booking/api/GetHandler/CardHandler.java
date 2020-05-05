c65e1ac971201a60f2603ec7fb5e6a60e57561f7
haijames <45039771+haijames@users.noreply.github.com>
2020-05-05 10:00:54 +0700

    Card Handler

---
+++ _Logic/JDBC/src/main/java/app/booking/api/GetHandler/CardHandler.java
@@ -0,0 +1,45 @@
+package app.booking.api.GetHandler;
+
+import app.booking.api.Constants;
+import app.booking.api.JsonConverter;
+import app.booking.api.ResponseEntity;
+import app.booking.api.StatusCode;
+import app.booking.db.Booking;
+import app.booking.db.SQLStatement;
+import app.booking.errors.GlobalExceptionHandler;
+import app.booking.slot.CitySlot;
+
+import java.io.InputStream;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CardHandler extends GetHandler {
+    public CardHandler(GlobalExceptionHandler exceptionHandler) {
+        super(exceptionHandler);
+    }
+
+    @Override
+    protected ResponseEntity doGet(InputStream is) throws Exception {
+        Map<String, List<String>> params = this.getParameters();
+
+        String PlayerId = params.get("id").get(0);
+        Date date = Date.valueOf(params.get("date").get(0));
+        // TODO: handle the case of missing/incorrect params
+
+        // Get cards of the player
+        ArrayList<Card> PlayerCard = SQLStatement.getPlayerCards(PlayerId);
+        //Check for usable card
+        for (Card c:PlayerCard
+             ) {
+            if (date.compareTo(c.exdate) > 0)
+                PlayerCard.remove(c);
+            else if (c.numb == 0) PlayerCard.remove(c);
+        }
+        // Convert to json
+        String response = JsonConverter.toJson(PlayerCard);
+
+        return new ResponseEntity<>(response, getHeaders(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON), StatusCode.OK);
+    }
+}
