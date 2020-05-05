a5250132879fc25803c7b28eb2b09ce9b6f67b33
Dang Cong <41923742+DangChiCongCS17@users.noreply.github.com>
2020-05-05 11:44:56 +0700

    Update cancelBooking.sql

--- _Database/Stored Procedures/cancelBooking.sql
+++ _Database/Stored Procedures/cancelBooking.sql
@@ -1,7 +1,7 @@
 -- cancelBooking(bookingId, playerId)
 DROP PROCEDURE IF EXISTS CancelBooking;
 DELIMITER //
-CREATE PROCEDURE CancelBooking(
+CREATE PROCEDURE CancelBooking(  
 in pbookingId varchar(50), 
 in pplayerId varchar(50),
 OUT resultCode varchar(50))
@@ -18,7 +18,7 @@ THEN SET resultCode = 'CA-004';
 ELSEIF date_add(date(now()), INTERVAL 24 HOUR) >
 									( SELECT date_add(date, INTERVAL TIME_TO_SEC(startTime) SECOND) 
 									  FROM booking 
-									  WHERE booking_id = pbookingId)
+									  WHERE booking_id = pbookingId) --Players can not cancel after 24 hours-booking--
 THEN SET resultCode = 'CA-005';
 ELSE 
 	DELETE FROM booking WHERE booking_id = pbookingId;
